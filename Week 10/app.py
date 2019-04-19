80ff0baca742745c78e8f84787a1dd01df61ec2b
Bethany Lindberg <bethfisch@gmail.com>
2019-04-19 16:48:29 -0500

    clean up app code

--- Week 10/app.py
+++ Week 10/app.py
@@ -1,6 +1,5 @@
 import numpy as np
 import datetime as dt
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
