55bbf7bc29c16ad611097a98ca2a3d703d10a1dc
Bethany Lindberg <bethfisch@gmail.com>
2019-05-02 18:12:33 -0500

    edits to templates, stylesheets and app

--- Week 12/app.py
+++ Week 12/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", data=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -25,14 +25,15 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
+    mars_data = scrape_mars.scrape_data()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
-
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
+    
     # Redirect back to home page
     return redirect("/")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
