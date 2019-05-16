bd4fd6bd228d5d9ba80ea997e60ba111cd60e0d2
Bethany Lindberg <bethfisch@gmail.com>
2019-05-16 16:59:14 -0500

    add more filters

--- Week 14/static/js/app.js
+++ Week 14/static/js/app.js
@@ -1,12 +1,13 @@
 // from data.js
 var tableData,tbody,filter,row,cell;
 tableData = data;
+// table elements variables
 tbody = d3.select("tbody");
 row = tbody.append("tr");
 cell = row.append("td");
-//Select filter button
+// Select filter button
 filter = d3.select('#filter-btn');
-//Populate table with all data
+// Populate table with all data
 tableData.forEach((UFOreport) => {
     row = tbody.append("tr");
     Object.entries(UFOreport).forEach(([key, value]) => {
@@ -15,19 +16,37 @@ tableData.forEach((UFOreport) => {
     });
   });
 
-//Click handler
+// Click handler
 filter.on("click",function(){
-    //Clear table
+    // Clear table
     tbody.html("");
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select(".form-control");
+    var inputElement = d3.selectAll(".form-control");
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-    //Filter Data by Input Value
-    filteredData = tableData.filter(date => date.datetime === inputValue);
-    //Append filtered data to table
+    var inputValue = []
+    inputElement.each(function(){
+        inputValue.push(this.value);
+    });
+    // Filter Data by Input Value
+    filteredData = tableData;
+    if (inputValue[0] != ''){
+        filteredData = filteredData.filter(sighting => sighting.datetime === inputValue[0]);
+    }
+    if (inputValue[1] != ''){
+        filteredData = filteredData.filter(sighting => sighting.city === inputValue[1].toLowerCase());
+    }
+    if (inputValue[2] != ''){
+        filteredData = filteredData.filter(sighting => sighting.state === inputValue[2].toLowerCase());
+    }
+    if (inputValue[3] != ''){
+        filteredData = filteredData.filter(sighting => sighting.country === inputValue[3].toLowerCase());
+    }
+    if (inputValue[4] != ''){
+        filteredData = filteredData.filter(sighting => sighting.shape === inputValue[4].toLowerCase());
+    }
+    // Append filtered data to table
     filteredData.forEach((UFOreport) => {
         row = tbody.append("tr");
         Object.entries(UFOreport).forEach(([key, value]) => {
