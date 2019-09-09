9ca06369c65e92e102e6fc1b97caef5da9cc0344
Bethany Lindberg <bethfisch@gmail.com>
2019-09-09 08:02:29 -0500

    mars app and py scripts

---
+++ 12_Mars_WebScrapingApp/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.mars_data.find_one()
+
+    # Return template and data
+    return render_template("index.html", data=mars_data)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_data()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
+    
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
